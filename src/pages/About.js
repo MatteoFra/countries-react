e68ba96cf1e0be00159d98e168eefa4c15bea1ad
MatteoFra <matteofra@outlook.fr>
2022-08-10 10:54:31 +0200

    final commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h2>about</h2>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur alias veniam corporis nesciunt eveniet, sunt mollitia dignissimos eum quisquam et ad maxime provident ipsa quae porro assumenda aliquid molestiae incidunt sed placeat ex voluptatum cumque! Earum dolorem, beatae dicta quasi odit repellendus nam! Eius labore repellendus soluta minima, quibusdam, hic qui eligendi ipsa sint, tempore veniam obcaecati reprehenderit quod magnam? Ratione laudantium natus dolor alias dolorum tenetur animi rem! Magni, laborum? A ipsam veritatis perferendis omnis voluptatem voluptates inventore, itaque eaque, pariatur illum quia labore eos quaerat expedita libero velit nostrum nulla, dolore explicabo hic laudantium vero praesentium ea voluptatibus! Magni corporis eos assumenda quas veritatis odio provident, nobis mollitia velit repudiandae accusantium. Unde excepturi consequatur ducimus voluptatibus modi consectetur itaque tempora molestias cum totam? Ex quia id modi nisi repudiandae nihil ut voluptatem, quasi velit perspiciatis. Neque esse sed voluptatum odit. Optio vitae exercitationem fugiat et excepturi nihil placeat at corporis. Voluptate corrupti doloribus cupiditate et placeat pariatur eos error repellendus quaerat, temporibus dignissimos illum aut, quos necessitatibus sunt fugiat magni numquam omnis provident. Natus, ducimus deserunt quo in atque iure pariatur maxime odio recusandae provident eum accusantium est eveniet neque omnis animi consequatur cum exercitationem sapiente incidunt itaque!
+            </p>
+        </div>
+    );
+};
+
+export default About;
